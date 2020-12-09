d57275f844b53bd125235bf98e4e9d84ada0968c
Matti Leinonen <moleinonen@gmail.com>
2020-12-09 18:07:50 +0200

    Add syntax highlighting to blog posts

--- src/utils/markdownConverter.ts
+++ src/utils/markdownConverter.ts
@@ -1,5 +1,22 @@
 import MarkdownIt from 'markdown-it';
-const md = MarkdownIt();
+import hljs from 'highlight.js';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+const md = MarkdownIt({
+  highlight: function (str, lang) {
+    if (lang && hljs.getLanguage(lang)) {
+      try {
+        return '<pre class="hljs"><code>' +
+               hljs.highlight(lang, str, true).value +
+               '</code></pre>';
+      } catch (e) {
+        console.log(e);
+      }
+    }
+
+    return '<pre class="hljs"><code>' + md.utils.escapeHtml(str) + '</code></pre>';
+  }
+});
 
 export const mdToHTML = (markdown: string): string => {
   if (!markdown) return '';
